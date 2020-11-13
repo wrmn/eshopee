14ada7661e1b40deb0a5bddaac11cda7fbdd12f5
aditya-wrmn <aditya_wrmn@protonmail.com>
2020-11-13 23:32:08 +0700

    transaksi

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table='transaksi';
+    public $timestamps = false;
+
+    public function transaksi_details(){
+        return $this->hasMany('App/Models/transaksi_detail');
+    }
+
+
+}
---
+++ database/factories/TransaksiDetailFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transaksi_detail;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TransaksiDetailFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaksi_detail::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
